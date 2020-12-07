3ac350de25800a4ebdb2e90c3698102b8e610966
Xin Liu <xinliu2861@gmail.com>
2020-12-07 15:50:55 +0800

    able to display added contents

--- RandomQuote/main.swift
+++ RandomQuote/main.swift
@@ -7,5 +7,35 @@
 
 import Foundation
 
-print("Hello, World!")
+var quoteList: [String] = []
+let maxChar = 30
+let quoteColor = "#C46243"
+let fontSize = 13
+let bitbarAPI = "| color=\(quoteColor) length=\(maxChar) size=\(fontSize)\n"
 
+func initializationQuoteList() {
+    quoteList.append("你们要逃避淫行。人所犯的,无论什么罪,都身子以外,惟有行淫的,是得罪自己的身子.")
+    quoteList.append("只是我告诉你们,凡看见妇女就动淫念的,这人心里已经与她犯奸淫了。若是你的右眼叫你跌倒,就剜出来丢掉,宁可失去百体中的一体,不叫全身丢在地狱里。若是右手叫你跌倒,就砍下来丢掉,宁可失去百体中的一体,不叫全身下入地狱。")
+}
+
+func processQuoteToSeveralLine(quoteSentence: String) -> String {
+    var result: String = quoteSentence
+    let lengthOfBitbarAPI = bitbarAPI.count
+    let lengthOfQuoteSentence = quoteSentence.count
+    var i = maxChar, n = maxChar
+    while n < lengthOfQuoteSentence {
+        let insertIndex = result.index(result.startIndex, offsetBy: i)
+        result.insert(contentsOf: bitbarAPI, at: insertIndex)
+        i += maxChar + lengthOfBitbarAPI
+        n += maxChar
+    }
+    result.append(bitbarAPI)
+    return result
+}
+
+initializationQuoteList()
+print("📖")
+print("---")
+print(processQuoteToSeveralLine(quoteSentence: quoteList[1]))
+print("---")
+print(processQuoteToSeveralLine(quoteSentence: quoteList[0]))
